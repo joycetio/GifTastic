96b3cc72b56a42954aa736c5f67fb69536107500
Marie Joyce Tio <joycetio@Joyces-MacBook-Pro.local>
2017-03-15 13:46:15 -0700

    update JS file for API link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
             console.log(this);
             console.log(animal);
 
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
                 animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
             // console.log(queryURL);
