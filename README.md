7336cd28dec71ca78692037bb449050eccbfd6a4
Marie Joyce Tio <joycetio@Joyces-MacBook-Pro.local>
2017-03-15 11:51:02 -0700

    update files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # GifTastic
+
+## Instructions: 
+1. Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics. 
+2. Your app should take the topics in this array and create buttons in your HTML. 
+3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page. 
+4. When the user clicks one of the still GIPHY images, the gif shoul animate. If the user clicks the gif again, it should stop playing. 
+5. Under every gif, display its rating. 
+    * This data is provided by the GIPHY API. 
+6. Add a form to your page that takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array that remakes the buttons on the page.
+
+##Description on how to use the app: 
+
+## Technologies Used: 
+* HTML 
+* Javascript
+* jQuery 
+* GIPHY API 
+* AJAX 
---
+++ assets/javascript/app.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+    var topics = ["puppies", "cat", "koalas", "panda", "bird", "hedgehog", "pig", "goat", "turtle", "chinchilla", "bunny"];
+    var topicBtn = "";
+    var userInput = "";
+    var userInputButton;
+
+    function createButton() {
+        for (var i = 0; i < topics.length; i++) {
+
+            topicBtn = $("<button class='btn btn-primary topic-buttons'>" + topics[i] + "</button>");
+            console.log(topicBtn);
+
+            $("#buttons-go-here").append(topicBtn);
+            // $("#buttons-go-here").append(userInput); 
+
+        };
+    };
+
+    createButton();
+
+    function displayGifs() {
+        $(".topic-buttons").on("click", function() {
+            // console.log("i've been clicked");
+
+            var animal = $(this).html();
+            console.log(this);
+            console.log(animal);
+
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+                animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+            // console.log(queryURL);
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                console.log(response);
+
+                var results = response.data;
+                console.log("results: ", results);
+
+                for (var i = 0; i < results.length; i++) {
+                    var animalDiv = $("<div>");
+
+                    var ratings = results[i].rating;
+                    // console.log(ratings); 
+
+                    var ratingHTML = $("<h2>").text("Rating: " + ratings);
+                    // console.log(ratingHTML);
+
+                    var animalGifs = $("<img class='gif'>");
+
+                    animalGifs.attr("src", results[i].images.fixed_height_still.url);
+                    animalGifs.attr("data-still", results[i].images.fixed_height_still.url);
+                    animalGifs.attr("data-animate", results[i].images.fixed_height.url);
+                    animalGifs.attr("data-state", "still");
+
+                    console.log(animalGifs); // console.log("results image still URL: ",results[i].images.fixed_height_still.url);
+
+
+                    animalDiv.append(ratingHTML);
+                    animalDiv.append(animalGifs);
+
+                    $("#gifs-go-here").prepend(animalDiv);
+                };
+
+                $(".gif").on("click", function() {
+                    var state = $(this).attr("data-state");
+                    // console.log("user click: ", this); 
+                    // console.log(state);
+
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                });
+
+            });
+        });
+    };
+
+    displayGifs();
+
+    $("#submitBtn").on("click", function() {
+        console.log("clicked");
+
+        var userInput = $("#animalUserInput").val().trim();
+
+        if ( userInput === ""){
+        	alert("You didn't enter an animal");
+
+        	$("#animalUserInput").val("");
+        }
+        else if ($.inArray(userInput, topics) != -1) {
+        	alert("Animal already exists");
+
+        	$("#animalUserInput").val("");
+        }
+        else {
+
+        userInputButton = $("<button class='btn btn-primary topic-buttons'>" + userInput + "</button>");
+        console.log(userInput);
+        console.log(userInputButton);
+
+        $("#buttons-go-here").append(userInputButton);
+
+        displayGifs();
+
+        $("#animalUserInput").val("");
+
+    	};
+
+    });
+
+
+});
